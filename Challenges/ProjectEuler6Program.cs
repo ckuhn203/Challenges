4898d25f18c274251bb1412e8ebe6c281654d063
Christopher J. Kuhn <ckuhn203@gmail.com>
2014-07-30 23:12:38 -0400

    Challenge 6

---
+++ Challenges/ProjectEuler6Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    class ProjectEuler6Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = Enumerable.Range(1, 100).ToList();
+            List<int> squares = new List<int>();
+
+            foreach (var i in numbers)
+            {
+                squares.Add(i * i);
+            }
+
+            long numberSum = numbers.Sum();
+            long temp = (numberSum * numberSum) - squares.Sum();
+            Console.WriteLine(temp);
+
+            Console.WriteLine();
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+
+
+    }
+}
