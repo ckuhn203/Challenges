04631ba37ac1d41aab4d819bd1edbf1d12031b3f
Christopher J. Kuhn <ckuhn203@gmail.com>
2014-08-09 20:19:22 -0400

    Project Euler 7

--- Challenges/MathExtensions.cs
+++ Challenges/MathExtensions.cs
@@ -104,7 +104,5 @@ namespace Challenges
             return result;
         }
          
-
-
     }
 }
---
+++ Challenges/ProjectEuler7Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Challenges
+{
+    class ProjectEuler7Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<long> primes = new List<long>();
+            long i = 1;
+
+            while (primes.Count < 10001)
+            {
+                if (MathExtensions.IsPrime(i))
+                {
+                    primes.Add(i);
+                }
+                i++;
+                    
+            }
+
+            Console.WriteLine(primes.Last());
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+    }
+}
